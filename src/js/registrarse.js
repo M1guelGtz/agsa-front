0e4c2e2edd8ffc9f63087b5afc238074032f60b2
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2025-11-24 11:33:43 -0600

    Register redirection to login test login hola

--- src/js/registrarse.js
+++ src/js/registrarse.js
@@ -123,7 +123,7 @@ formRegistro.addEventListener('submit', (e) => {
                 if(res.ok){
                 mostrarMensaje('Registro exitoso. Redirigiendo al inicio de sesión...', 'exito');
                 formRegistro.reset();
-                setTimeout(()=> window.location.href='/', 1400);
+                setTimeout(()=> window.location.href='/ags-front/index.html', 1400);
                 return;
             } else {
                 const msg = (data && (data.message || data.error)) ? (data.message || data.error) : `Error ${res.status}`;
